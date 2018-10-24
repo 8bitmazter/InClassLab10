6276b3098b20340a9a4c6d041c2dcf4e60035341
Seano <seanoliverrobinson@gmail.com>
2018-10-23 20:40:41 -0400

    First Upload

---
+++ InClassLab10/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassLab10
+{
+    class Circle //Circle class, circumference & area
+    {
+        private double area, radius;
+        public const double PI = Math.PI;
+
+        public double Radius //constructor 
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+
+        public Circle(double radius)
+        {
+            this.radius = radius; //initializing the variable
+        }
+        public string FormatNumber(double x)
+        {
+            return $"{x,0:N2}";
+        }
+        public double CalculateCircumference()
+        {
+            double circumference = PI * (radius * 2); //calculate circumference equation
+            return circumference;
+        }
+        //Allow for user input on number of decimal places
+        public string CaclculateFormattedCircumference()
+        {
+            double formatted = Math.Round(CalculateCircumference(), 2); //using math round to format the circumference 
+            return formatted.ToString(); //every object has a  .ToString  change
+        }
+        public double CalculateArea()
+        {
+            double area = PI * (radius * radius); //calculate area
+            return area;
+        }
+        public string CalculateFormattedArea()
+        {
+            double formattedArea = Math.Round(CalculateArea(), 2); //using math round to format the area
+            return formattedArea.ToString();
+        }
+    }
+}
---
+++ InClassLab10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassLab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0; //using for the counter
+            bool repeat = true;  //using to loop the code
+            while (repeat == true)
+            {
+                Console.WriteLine("Please enter the radius of a circle!");
+                double UserInput = double.Parse(Console.ReadLine());
+                Circle myCicle = new Circle(UserInput); //called circle class and named this string "myCircle"
+
+                Console.WriteLine($"Circumference: {myCicle.CalculateCircumference()}"); //used the $ to replace the {0} placement holder
+                Console.WriteLine($"Circumference Formatted! {myCicle.CaclculateFormattedCircumference()}"); //see Circle Class for steps
+
+                Console.WriteLine($"Area: {myCicle.CalculateArea()}");
+                Console.WriteLine($"Area formatted! {myCicle.CalculateFormattedArea()}"); //see circle Class for steps
+                c++;
+                Console.ReadLine();
+                while (true) //used bool for breaking out of the continue loop
+                {
+                    Console.WriteLine("Would you like to continue? (Y/N)");
+                    string again = Console.ReadLine().ToLower();
+                    if (again == "y")
+                    {
+                        repeat = true;
+                        break;
+                    }
+                    if (again == "n")
+                    {
+                        repeat = false;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please choose Y/N"); //using incase user does not use y or n
+                        continue;
+                    }
+                }
+                Console.WriteLine($"You ran this program { c }");
+
+            }
+        }
+    }
+}
